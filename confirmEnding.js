abcf7fad7ceef73c31a469753317a846be364835
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-10-09 07:57:40 +0100

    Solve the challenge confirm end with the endsWith() method

--- confirmEnding.js
+++ confirmEnding.js
@@ -13,3 +13,18 @@ console.log(confirmEnding('He has to give me a new name', 'name'));
 console.log(confirmEnding('I intend becoming great in life', 'in'));
 console.log(confirmEnding('Qoosim is my friend', 'friend'));
 console.log(confirmEnding('Alaje is my family name', 'name')); 
+
+console.log('=============')
+
+const confirmWithEndsWithMethod = (str, target) => {
+    if (str.endsWith(target)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(confirmWithEndsWithMethod('Qoosim', 'm'));
+console.log(confirmWithEndsWithMethod('You are too much', 'much'));
+console.log(confirmWithEndsWithMethod('Adekunle is my third name', 'third'));
+
