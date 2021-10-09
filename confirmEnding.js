ee4d700e0e4c885b318abcf2806ae071aaddf478
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-10-09 06:54:46 +0100

    Solve a challenge that return true if the end of a given string is equal to the second parameter of the function that determines that

--- confirmEnding.js
+++ confirmEnding.js
@@ -1,8 +1,15 @@
 const confirmEnding = (str, target) => {
-    return str.substring(str.length - target.length);
+    if (str.substring(str.length - target.length) === target) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(confirmEnding('Bastian', 'n'));
 console.log(confirmEnding('court', 'm'));
 console.log(confirmEnding('congratulation', 'on'));
-//console.log(confirmEnding('He has to give me a new name', 'name'));
+console.log(confirmEnding('He has to give me a new name', 'name'));
+console.log(confirmEnding('I intend becoming great in life', 'in'));
+console.log(confirmEnding('Qoosim is my friend', 'friend'));
+console.log(confirmEnding('Alaje is my family name', 'name')); 
