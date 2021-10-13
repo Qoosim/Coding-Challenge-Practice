abd1d2713512681efccd1654b78d7c770befe0bc
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-10-13 22:35:57 +0100

    Use more methods to solve the same challenge

--- bouncer.js
+++ bouncer.js
@@ -1,13 +1,26 @@
-function bouncer(arr) {
-    const newArr = arr.filter(function(elem) {
-        if (elem) {
-            return elem;
+function bouncer1(arr) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) {
+            newArr.push(arr[i]);
         }
-    });
+    }
     return newArr;
 }
 
-console.log(bouncer([7, 'ate', '', false, 9]));
-console.log(bouncer(["a", "b", "c"]));
-console.log(bouncer([false, null, 0, NaN, undefined, ""]));
-console.log(bouncer([null, NaN, 1, 2, undefined]));
+function bouncer2(arr) {
+    return arr.filter(function(value) {
+        if (value) {
+            return value;
+        }
+    })
+}
+
+function bouncer3(arr) {
+    return arr.filter(Boolean);
+}
+
+console.log(bouncer1([7, 'ate', '', false, 9]));
+console.log(bouncer2(["a", "b", "c", null, 0.0]));
+console.log(bouncer2([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer3([null, NaN, 1, 2, undefined]));
